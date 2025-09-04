cef27a280e58f27df4f4b76d17d7a8efb6c340c3
Guntur W <ggm5850@gmail.com>
2025-09-04 11:39:05 +0700

    feat: add address

--- js/register.js
+++ js/register.js
@@ -14,35 +14,35 @@ document.getElementById("registerForm").addEventListener("submit", (e) => {
       icon: "error",
     });
   }
-  if(email.length <= 0){
+  if (email.length <= 0) {
     e.target.email.focus();
     return Swal.fire({
       title: "Email tidak boleh kosong!",
       icon: "error",
     });
   }
-  if(username.length <= 0){
+  if (username.length <= 0) {
     e.target.username.focus();
     return Swal.fire({
       title: "Username tidak boleh kosong!",
       icon: "error",
     });
   }
-  if(password.length < 8){
+  if (password.length < 8) {
     e.target.password.focus();
     return Swal.fire({
       title: "Password tidak boleh kurang dari 8!",
       icon: "error",
     });
   }
-  if(passwordConfirmation.length < 8){
+  if (passwordConfirmation.length < 8) {
     e.target.passwordConfirmation.focus();
     return Swal.fire({
       title: "Password tidak boleh kurang dari 8!",
       icon: "error",
     });
   }
-  
+
   if (password != passwordConfirmation) {
     return Swal.fire({
       title: "Password tidak sama!",
@@ -50,7 +50,6 @@ document.getElementById("registerForm").addEventListener("submit", (e) => {
     });
   }
 
-  
   /** Handle user duplicate */
   const allUsers = getUsers();
   const user = allUsers.find(
@@ -78,7 +77,7 @@ document.getElementById("registerForm").addEventListener("submit", (e) => {
   return Swal.fire({
     title: "Pendaftaran berhasil!",
     icon: "success",
-  }).then(()=> {
+  }).then(() => {
     window.location.href = `/`;
   });
 });
@@ -110,7 +109,6 @@ document.querySelectorAll(".passwordInput").forEach((elem) => {
   });
 });
 
-
 function addErrorInputPassword() {
   document.querySelectorAll(".error-msg").forEach((element) => {
     element.classList.remove("hidden");
@@ -128,3 +126,74 @@ function removeErrorInputPassword() {
     element.classList.remove("error");
   });
 }
+
+const API_BASE_URL = "https://www.emsifa.com/api-wilayah-indonesia/api";
+
+const getProvinces = () => {
+  return axios.get(`${API_BASE_URL}/provinces.json`);
+};
+
+const getRegencies = (provinceId) => {
+  return axios.get(`${API_BASE_URL}/regencies/${provinceId}.json`);
+};
+
+const getDistricts = (regencyId) => {
+  return axios.get(`${API_BASE_URL}/districts/${regencyId}.json`);
+};
+
+const getVillages = (districtId) => {
+  return axios.get(`${API_BASE_URL}/villages/${districtId}.json`);
+};
+
+window.addEventListener("load", async function () {
+  const provEl = document.getElementById("prov");
+  const kabEl = document.getElementById("kab");
+  const kecEl = document.getElementById("kec");
+  const desaEl = document.getElementById("desa");
+
+  const provs = (await getProvinces()).data;
+
+  provs.forEach((prov) => {
+    const opt = document.createElement("option");
+    opt.setAttribute("value", prov.id);
+    opt.textContent = prov.name;
+    provEl.appendChild(opt);
+  });
+  provEl.removeAttribute("disabled");
+
+  provEl.addEventListener("change", async function (e) {
+    const kabId = e.target.value;
+    const kabs = (await getRegencies(kabId)).data;
+    kabs.forEach((kab) => {
+      const opt = document.createElement("option");
+      opt.setAttribute("value", kab.id);
+      opt.textContent = kab.name;
+      kabEl.appendChild(opt);
+    });
+    kabEl.removeAttribute("disabled");
+  });
+
+  kabEl.addEventListener("change", async function (e) {
+    const kecId = e.target.value;
+    const kecs = (await getDistricts(kecId)).data;
+    kecs.forEach((kec) => {
+      const opt = document.createElement("option");
+      opt.setAttribute("value", kec.id);
+      opt.textContent = kec.name;
+      kecEl.appendChild(opt);
+    });
+    kecEl.removeAttribute("disabled");
+  });
+
+  kecEl.addEventListener("change", async function (e) {
+    const desaId = e.target.value;
+    const desas = (await getVillages(desaId)).data;
+    desas.forEach((desa) => {
+      const opt = document.createElement("option");
+      opt.setAttribute("value", desa.id);
+      opt.textContent = desa.name;
+      desaEl.appendChild(opt);
+    });
+    desaEl.removeAttribute("disabled");
+  });
+});
