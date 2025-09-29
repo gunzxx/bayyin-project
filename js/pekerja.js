3984b06dce4e13783afb1d9364a483c167e2018c
Guntur W <ggm5850@gmail.com>
2025-09-30 01:17:16 +0700

    feat: change edit profile to profile

--- js/pekerja.js
+++ js/pekerja.js
@@ -4,8 +4,20 @@ function convertMinutes(minutes) {
   return `${hours} jam ${remainingMinutes} menit`;
 }
 
-
+/** Handle user data */
 const datas = [
+  {
+    layanan: "WiFi",
+    tanggal: Date.now(),
+    pengguna: "Budi",
+    alamat: "Jl. Anggrek No.2",
+    catatan: "Perlu kabel tambahan",
+    phone: "+6281234567890",
+    startDate: '',
+    endDate: '',
+    durasi: "-",
+    status: null,
+  },
   {
     layanan: "WiFi",
     tanggal: Date.now(),
@@ -13,6 +25,8 @@ const datas = [
     alamat: "Jl. Cemara No.23",
     catatan: "Perlu kabel tambahan",
     phone: "+6281234567890",
+    startDate: '',
+    endDate: '',
     durasi: convertMinutes(100),
     status: "selesai",
   },
@@ -23,12 +37,29 @@ const datas = [
     alamat: "Jl. Bunga Raya No.12",
     catatan: "Kamera outdoor",
     phone: "+6281234567890",
+    startDate: '',
+    endDate: '',
     durasi: "-",
-    status: "dikerjakan",
+    status: "proses",
   },
 ];
 
-datas.forEach((data) => {
+/** Handle user data */
+function updateByIndex(idx, col, val){
+  if (datas[idx]){
+    if(datas[idx][col]){
+      datas[idx][col] = val
+      return true
+    }
+    return false
+  }
+  return false
+}
+
+
+
+/** Making data table */
+datas.forEach((data, idx) => {
   const rowElement = document.createElement("tr");
 
   const col1 = document.createElement("td");
@@ -64,36 +95,72 @@ datas.forEach((data) => {
 
   const col7 = document.createElement("td");
   const col7Val = document.createElement("div");
-  col7Val.innerText = capitalizeFirst(data["status"]);
+  col7Val.innerText = capitalizeFirstStatus(data["status"]);
   col7Val.classList.add(data['status'])
   col7.appendChild(col7Val);
 
+  function getTextButton(){
+    if(!data['status']){
+      return 'mulai'
+    }else if(data['status'] == 'proses'){
+      return 'selesaikan'
+    }else if(data['status'] == 'selesai'){
+      return 'selesai'
+    }
+  }
+
+  function getBgButton(){
+    if(!data['status']){
+      return 'bg-green-500'
+    }else if(data['status'] == 'proses'){
+      return 'bg-red-500'
+    }else if(data['status'] == 'selesai'){
+      return 'bg-gray-500'
+    }
+  }
+
   const col8 = document.createElement("td");
   const col8Val = document.createElement("button");
-  col8Val.innerText = capitalizeFirst(data['status'] == "selesai" ? "selesai" : "selesaikan");
-  col8Val.classList.add("border-none", "py-3", "px-4", "rounded-md", "text-white")
-  col8Val.classList.add(data['status'] == 'selesai' ? "bg-gray-500" : "bg-red-500")
+  col8Val.innerText = capitalizeFirst(getTextButton());
+  col8Val.classList.add("w-full", "min-w-max", "border-none", "py-3", "px-4", "rounded-md", "text-white")
+  col8Val.classList.add(getBgButton())
 
   function handleClick(){
-    Swal.fire({
-      title: "Selesaikan pekerjaan?",
-      showCancelButton: true,
-      confirmButtonColor: "red",
-    }).then((result) => {
-      if(result.isConfirmed){
-        col8Val.parentElement.previousElementSibling.querySelector("div").innerText = capitalizeFirst("selesai")
-        col8Val.parentElement.parentElement.querySelector(".durasi-col").innerText = capitalizeFirst(convertMinutes(200))
-        col8Val.classList.remove("bg-red-500")
-        col8Val.classList.add("bg-gray-500")
-        col8Val.innerText = capitalizeFirst("selesai")
-        col8Val.classList.remove("cursor-pointer")
-      }
-    });
-
-    col8Val.removeEventListener("click", handleClick);
+    if (!data['status']){
+      return Swal.fire({
+        title: "Mulai pekerjaan?",
+        showCancelButton: true,
+        confirmButtonColor: "red",
+      }).then((result) => {
+        if(result.isConfirmed){
+          col8Val.parentElement.previousElementSibling.querySelector("div").innerText = capitalizeFirst("proses")
+          col8Val.classList.remove("bg-green-500")
+          col8Val.classList.add("bg-red-500")
+          col8Val.innerText = capitalizeFirst("selesaikan")
+          col8Val.removeEventListener("click", handleClick);
+        }
+      });
+    } else if (data['status'] == 'proses'){
+      
+      return Swal.fire({
+        title: "Selesaikan pekerjaan?",
+        showCancelButton: true,
+        confirmButtonColor: "red",
+      }).then((result) => {
+        if(result.isConfirmed){
+          col8Val.parentElement.previousElementSibling.querySelector("div").innerText = capitalizeFirst("selesai")
+          col8Val.parentElement.parentElement.querySelector(".durasi-col").innerText = capitalizeFirst(convertMinutes(200))
+          col8Val.classList.remove("bg-red-500")
+          col8Val.classList.add("bg-gray-500")
+          col8Val.innerText = capitalizeFirst("selesai")
+          col8Val.classList.remove("cursor-pointer")
+          col8Val.removeEventListener("click", handleClick);
+        }
+      });
+    }
   }
   
-  data['status'] == "selesai" ? "" : col8Val.classList.add("cursor-pointer")
+  col8Val.classList.add("cursor-pointer")
   data['status'] == "selesai" ? col8Val.setAttribute("disabled", true) : ""
   data['status'] == "selesai" ? "" : col8Val.addEventListener("click", handleClick)
   col8.appendChild(col8Val);
@@ -111,5 +178,12 @@ datas.forEach((data) => {
 });
 
 function capitalizeFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function capitalizeFirstStatus(str) {
+  if (!str){
+    return "Belum dimulai"
+  }
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
